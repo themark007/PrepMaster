a84fd119131e5d9d20599137028673d3fc2a6d3c
mark dsouza <mark.desouza2003@gmail.com>
2025-08-16 12:03:27 +0530

    render push backend

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://prepmaster-frontend.onrender.com", credentials: true }));
 
 
 
